d821256047bb154a11ed18504c0a5a2ddf817be5
benhaim23 <markbenhaim0@gmail.com>
2025-02-26 23:37:04 -0500

    Added demonstration of real-time GitHub sync

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 def hello():
+    # This is a test change to demonstrate GitHub synchronization
     print('Hello, GitHub!')
+    print('This change will be pushed to GitHub!')
 
 if __name__ == '__main__':
     hello()
